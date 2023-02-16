e32f5e10cf63a3a081e62178648090e2a0c9dbba
azikkw <94234353+azikkw@users.noreply.github.com>
2023-02-16 11:31:08 +0600

    ToDo Update 2

--- Lab3/ToDo/main.js
+++ Lab3/ToDo/main.js
@@ -2,6 +2,7 @@ const input = document.querySelector('.text_area');
 const saveBtn = document.querySelector('.save_goal');
 
 const toDoList = document.querySelector('.to_do_list');
+const listBack = document.querySelector('.list_back');
 const errorMessage = document.querySelector('.error');
 const greetengMessage = document.querySelector('.greeteng');
 
@@ -17,7 +18,7 @@ saveBtn.addEventListener('click', () => {
                     </div>`;
 
     if(input.value.length !== 0) {
-        toDoList.insertAdjacentHTML("beforeend", blockList);
+        listBack.insertAdjacentHTML("beforeend", blockList);
         input.value = "";
     } 
     else {
@@ -28,11 +29,90 @@ saveBtn.addEventListener('click', () => {
     }
 });
 
+const trash = document.querySelector('.trash');
+const trashBack = document.querySelector('.trash_back');
+const trashOpen = document.querySelector('.trash_open_back');
+const trashClose = document.querySelector('.trash_close');
+const trashList = document.querySelector('.trash_list');
+const trashNoEmpty = document.querySelector('.no_empty');
+const trashIsEmpty = document.querySelector('.its_empty');
+
+trashOpen.addEventListener('click', () => {
+    if(trashList.childElementCount > 0) {
+        trash.classList.add('open');
+        trashBack.classList.add('open');
+    } 
+    else {
+        trashIsEmpty.classList.add('show');
+        setTimeout( () => { 
+            trashIsEmpty.classList.remove('show');
+        }, 2500);
+    }
+});
+trashClose.addEventListener('click', () => {
+    trash.classList.remove('open');
+    trashBack.classList.remove('open');
+});
+trashBack.addEventListener('click', () => {
+    trash.classList.remove('open');
+    trashBack.classList.remove('open');
+});
+
 toDoList.addEventListener('click', (e) => {
     if(e.composedPath()[1].className === 'delete_block') {
+        trashList.insertAdjacentHTML("beforeend", `<div class="trash_block">
+                                                    <div class="reload">
+                                                        <img src="reload.svg" alt="">
+                                                    </div>
+                                                    <div class="block_list">
+                                                        ${e.composedPath()[2].innerHTML}
+                                                    </div>
+                                                </div>`);
+
         e.composedPath()[2].remove();
+    }
+    else if(e.composedPath()[0].className === 'remove_all_btn') {
+        for(let i = 0; i < listBack.childElementCount; i++) {
+            while(listBack.childElementCount != 0) {
+                trashList.insertAdjacentHTML("beforeend", `<div class="trash_block">
+                                                            <div class="reload">
+                                                                <img src="reload.svg" alt="">
+                                                            </div>
+                                                            <div class="block_list">
+                                                                ${listBack.childNodes[i].innerHTML}
+                                                            </div>
+                                                        </div>`);
+
+                listBack.childNodes[i].remove();
+            }
+        }
+    }
+
+    if(trashList.childElementCount > 0) {
+        trashNoEmpty.classList.add('show');
     }
 });
 
-// var fs = require('fs');
-// fs.writeFileSync()
+trash.addEventListener('click', (e) => {
+    if(e.composedPath()[1].className === 'reload') {
+        listBack.insertAdjacentHTML("beforeend", `<div class="block_list">
+                                                    ${e.composedPath()[2].childNodes[3].innerHTML}
+                                                </div>`);
+
+        e.composedPath()[2].remove();
+    }
+    else if(e.composedPath()[1].className === 'delete_block') {
+        e.composedPath()[3].remove();
+    }
+    else if(e.composedPath()[0].className == 'clear_all') {
+        for(let i = 0; i < trashList.childElementCount; i++) {
+            while(trashList.childElementCount != 0) {
+                trashList.childNodes[i].remove();
+            }
+        }
+    }
+
+    if(trashList.childElementCount < 1) {
+        trashNoEmpty.classList.remove('show');
+    }
+});
