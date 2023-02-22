00f60e49cd16573ed3ea9ff25545c21f754ec002
PP2-21B030774 <azat.amenov@gmail.com>
2023-02-22 21:18:39 +0600

    Lab 4 Update 2.0

--- Lab4/online-store/src/app/products.ts
+++ Lab4/online-store/src/app/products.ts
@@ -10,18 +10,28 @@ export const products = [
     id: 1,
     name: 'Apple iPhone 14 Pro Max 1Tb фиолетовый',
     price: 966323,
-    description: 'A large phone with one of the best screens',
+    description: 'Front camera: 12 MP, 23 mm (eq.), f/1.9; support 4G and 5G, Wi-Fi 6, Bluetooth 5.0, Lightning; A-GPS, GLONASS, GALILEO, BDS, QZSS; battery 4323 mAh, non-removable, supports fast wired charging 18 W, wireless charging MagSafe 15 watts.',
     img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hda/ha0/62948782342174/apple-iphone-14-pro-max-128gb-fioletovyj-106363345-1.jpg'
   },
   {
     id: 2,
     name: 'Apple MacBook Pro 13 MNEH3 серый',
     price: 698959,
-    description: 'A great phone with one of the best cameras',
+    description: 'The first chip designed specifically for Mac. Amazingly, the system on the Apple M1 chip accommodates 16 billion transistors and combines the central and graphics processors, the Neural Engine system, I/O controllers and many other components.',
     img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h37/hd2/51918241038366/apple-macbook-pro-13-mneh3-seryi-105514238-1.jpg'
   },
   {
     id: 3,
     name: 'Apple AirPods Max розовый',
     price: 369990,
-    description
+    description: 'The Apple AirPods Max Bluetooth headset will be able to completely change your idea of full-size headphones. This model has been developed using first-class materials and advanced technologies. It is based on the Apple H1 processor and proprietary dynamic drivers, among the advantages of which high operating parameters can be noted.',
+    img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h3d/he5/33881190039582/apple-airpods-max-rozovyj-101161561-1-Container.jpg'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
