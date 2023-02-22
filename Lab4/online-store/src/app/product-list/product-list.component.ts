75b5f662958d94e7c12c51309d21fbc4f97667d8
PP2-21B030774 <azat.amenov@gmail.com>
2023-02-22 21:46:21 +0600

    Lab4 Update 3.0

--- Lab4/online-store/src/app/product-list/product-list.component.ts
+++ Lab4/online-store/src/app/product-list/product-list.component.ts
@@ -10,8 +10,12 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product: {kaspi: string}) {
+    window.open('https://telegram.me/share/url?url=' + product.kaspi)
+  }
+  
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
