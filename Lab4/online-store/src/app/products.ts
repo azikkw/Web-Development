587bd5f06c1e82533bf8b0de3ae201f3a4045d7c
PP2-21B030774 <azat.amenov@gmail.com>
2023-02-22 21:07:16 +0600

    Lab4 Update

---
+++ Lab4/online-store/src/app/products.ts
@@ -0,0 +1,27 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 14 Pro Max 1Tb фиолетовый',
+    price: 966323,
+    description: 'A large phone with one of the best screens',
+    img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hda/ha0/62948782342174/apple-iphone-14-pro-max-128gb-fioletovyj-106363345-1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Apple MacBook Pro 13 MNEH3 серый',
+    price: 698959,
+    description: 'A great phone with one of the best cameras',
+    img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h37/hd2/51918241038366/apple-macbook-pro-13-mneh3-seryi-105514238-1.jpg'
+  },
+  {
+    id: 3,
+    name: 'Apple AirPods Max розовый',
+    price: 369990,
+    description
