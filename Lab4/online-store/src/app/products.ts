d5281c064c843dec12e1f24bc851f06c01fad061
PP2-21B030774 <azat.amenov@gmail.com>
2023-02-22 21:57:45 +0600

    Lab4 Update 4.0

--- Lab4/online-store/src/app/products.ts
+++ Lab4/online-store/src/app/products.ts
@@ -20,7 +20,7 @@ export const products = [
     price: 698959,
     description: 'The first chip designed specifically for Mac. Amazingly, the system on the Apple M1 chip accommodates 16 billion transistors and combines the central and graphics processors, the Neural Engine system, I/O controllers and many other components.',
     img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h37/hd2/51918241038366/apple-macbook-pro-13-mneh3-seryi-105514238-1.jpg',
-    kaspi: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-1tb-fioletovyi-106363345/?c=750000000'
+    kaspi: 'https://kaspi.kz/shop/p/apple-macbook-pro-13-mneh3-seryi-105514238/?c=750000000#!/item'
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ export const products = [
     price: 369990,
     description: 'The Apple AirPods Max Bluetooth headset will be able to completely change your idea of full-size headphones. This model has been developed using first-class materials and advanced technologies. It is based on the Apple H1 processor and proprietary dynamic drivers, among the advantages of which high operating parameters can be noted.',
     img: 'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h3d/he5/33881190039582/apple-airpods-max-rozovyj-101161561-1-Container.jpg',
-    kaspi: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-1tb-fioletovyi-106363345/?c=750000000'
+    kaspi: 'https://kaspi.kz/shop/p/apple-airpods-max-rozovyi-101161561/?c=750000000#!/item'
   }
 ];
 
