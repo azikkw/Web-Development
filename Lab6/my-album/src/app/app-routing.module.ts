400591df22da9f991564b274b24f8bd0bc8cf78b
PP2-21B030774 <azat.amenov@gmail.com>
2023-03-07 23:57:26 +0600

    Lab6 Update 1.0

--- Lab6/my-album/src/app/app-routing.module.ts
+++ Lab6/my-album/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomePageComponent } from './home-page/home-page.component';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot ([
+      { path: '', component: HomePageComponent },
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Lab6/my-album/src/app/app.module.ts
+++ Lab6/my-album/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { AlbumsPageComponent } from './albums-page/albums-page.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
+@NgModule ({
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    AlbumsPageComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
