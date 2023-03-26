f77abca9400f5e10d442d50fae9e6275ff542644
PP2-21B030774 <azat.amenov@gmail.com>
2023-03-26 15:07:13 +0600

    Lab7 HackerRank 8, 9, 10

--- Lab7/HackerRank/10.py
+++ Lab7/HackerRank/10.py
@@ -1,7 +1,21 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
+def count_substring(string, sub_string):
+    count, i, j = 0, 0, len(sub_string)
     
-    print(a + b)
-    print(a - b)
-    print(a * b)
+    while True:
+        if string[i : j] == sub_string:
+            count = count + 1
+        
+        i += 1
+        j += 1
+        
+        if j > len(string):
+            break
+
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
--- Lab7/HackerRank/9.py
+++ Lab7/HackerRank/9.py
@@ -1,7 +1,20 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
+def mutate_string(string, position, character):
+    list = []
     
-    print(a + b)
-    print(a - b)
-    print(a * b)
+    for i in string:
+        list.append(i)
+    
+    list.pop(position)
+    list.insert(position, character)
+    
+    ans = ''
+    for i in list:
+        ans += i
+    
+    return ans
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
