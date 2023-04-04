752be4dbb20269e07e5b536f57bf725e7a517740
PP2-21B030774 <azat.amenov@gmail.com>
2023-04-04 22:14:50 +0600

    Lab8 Update 3.0

--- Lab8/shopback/api/models.py
+++ Lab8/shopback/api/models.py
@@ -8,6 +8,22 @@ class Product(models.Model):
     count = models.IntegerField(default=0)
     is_active = models.BooleanField()
 
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active
+        }
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
--- Lab8/shopback/api/views.py
+++ Lab8/shopback/api/views.py
@@ -1,21 +1,37 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 
+from api.models import Product, Category
+
 
 def products_list(request):
-    pass
+    products = Product.objects.all()
+    products_json = [p.to_json() for p in products]
+    return JsonResponse(products_json, safe=False)
 
 
-def product_detail(request):
-    pass
+def product_detail(request, id):
+    products = Product.objects.all()
+    products_json = [p.to_json() for p in products]
+    for pj in products_json:
+        if pj['id'] == id:
+            return JsonResponse(pj)
+    return JsonResponse({'error': 'Product not found!'})
 
 
 def categories_list(request):
-    pass
+    categories = Category.objects.all()
+    categories_json = [c.to_json() for c in categories]
+    return JsonResponse(categories_json, safe=False)
 
 
-def category_detail(request):
-    pass
+def category_detail(request, id):
+    categories = Category.objects.all()
+    categories_json = [c.to_json() for c in categories]
+    for cj in categories_json:
+        if cj['id'] == id:
+            return JsonResponse(cj)
+    return JsonResponse({'error': 'Category not found!'})
 
 
 def category_products_list(request):
